2a25f3c6b6125efc9f2c8e560859a6fb49af4095
vamsi krishna <vamsi.562@gmail.com>
2026-08-26 21:07:13 +0530

    updated bootstrap script

--- 40-database/database.tf
+++ 40-database/database.tf
@@ -16,16 +16,19 @@ resource "terraform_data" "mongodb" {
   triggers_replace = [
     aws_instance.mongodb.id
   ]
+
   connection {
     type     = "ssh"
     user     = "ec2-user"
     password = "DevOps321"
     host     = aws_instance.mongodb.private_ip
   }
+
   provisioner "file" {
     source      = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
   }
+
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
