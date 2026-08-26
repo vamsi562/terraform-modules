b44fd52f0ad6f3a4638e2f35347c513e670893e3
vamsi krishna <vamsi.562@gmail.com>
2026-08-26 22:11:50 +0530

    updated mysql db

--- 40-database/database.tf
+++ 40-database/database.tf
@@ -117,3 +117,42 @@ resource "terraform_data" "rabbitmq" {
   }
 }
 
+#------------------------------- mysql -----------------------------------------#
+resource "aws_instance" "mysql" {
+  ami                    = local.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id              = local.subnet_id
+  tags = merge(local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-mysql"
+    },
+    var.ec2_tags
+  )
+
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh mysql"
+    ]
+  }
+}
