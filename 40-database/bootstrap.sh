6ec05d609aa4d04315ef383fddbf6eb499cbca33
vamsi krishna <vamsi.562@gmail.com>
2026-08-26 21:36:42 +0530

    updated sg rules

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-component=$1
 dnf install ansible -y
 
-ansible-pull -U https://github.com/vamsi562/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+ansible-pull -U https://github.com/vamsi562/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
